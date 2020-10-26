78665b6dc28be36c96c00f248b8e9ad87a743c5b
loveisgala <gabriela.gk@hotmail.com>
2020-10-26 02:04:50 -0300

    feat: add validate function

--- main.js
+++ main.js
@@ -2,30 +2,37 @@
     "use strict";
 
     document.addEventListener('DOMContentLoaded', function() {
-
         var submit = document.getElementById('btnCheck');
         submit.addEventListener('click', palindrome);
 
         function palindrome(event) {
             event.preventDefault();
-
-
-
             var str = document.getElementById('check').value;
             var car = /[\W_]/g;
             var lowerStr = str.toLowerCase().replace(car, '');
             var reverseStr = lowerStr.split('').reverse().join('');
 
             if (reverseStr === lowerStr) {
-                document.getElementById('isornot').innerHTML = "yes";
+                document.getElementById('isornot').innerHTML = "is a palindrome :)";
                 document.getElementById('form').reset();
             } else {
-                document.getElementById('isornot').innerHTML = "no";
+                document.getElementById('isornot').innerHTML = "is not a palindrome :(";
                 document.getElementById('form').reset();
+            }
+        }
+
+        var isornot = document.getElementById('isornot');
+        var check = document.getElementById('check');
+        check.addEventListener('blur', validate);
+
+        function validate() {
+            if (this.value == '') {
+                isornot.style.display = "none";
+            } else {
+                isornot.style.display = "block";
             }
 
         }
-
 
     }); //DOM CONTENT LOADED
 })();
